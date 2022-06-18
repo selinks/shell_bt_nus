2ed46e0b7eeb3f8ba8336de928be98b95389eef8
Konstantin Selin <hoverlauk2061@gmail.com>
2022-06-18 16:42:26 +0300

    Added licensing information.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 /*
+ * Copyright (c) 2022 Konstantin Selin
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ * SPDX-License-Identifier: MIT-Modern-Variant
  */
 
 /** @file
